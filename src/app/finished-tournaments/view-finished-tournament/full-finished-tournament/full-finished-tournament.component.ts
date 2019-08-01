73ee2f1273b165c8bf443fc7adfa0b2d67f43cad
nrehder <nrehder@gmail.com>
2019-08-01 16:51:06 -0400

    full tournament history implemented

--- src/app/finished-tournaments/view-finished-tournament/full-finished-tournament/full-finished-tournament.component.ts
+++ src/app/finished-tournaments/view-finished-tournament/full-finished-tournament/full-finished-tournament.component.ts
@@ -1,15 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Observable } from "rxjs";
+import { DocumentData } from "@angular/fire/firestore";
+import { ActivatedRoute, Params } from "@angular/router";
+import { DatabaseService } from "src/app/shared/database.service";
+import { take } from "rxjs/operators";
+import { tournament } from "src/app/shared/tournament.model";
 
 @Component({
-  selector: 'app-full-finished-tournament',
-  templateUrl: './full-finished-tournament.component.html',
-  styleUrls: ['./full-finished-tournament.component.css']
+    selector: "app-full-finished-tournament",
+    templateUrl: "./full-finished-tournament.component.html",
+    styleUrls: ["./full-finished-tournament.component.css"],
 })
 export class FullFinishedTournamentComponent implements OnInit {
+    tournName: string;
+    tournId: number;
+    finishedTournaments: Observable<DocumentData[]>;
+    results: boolean[] = [];
+    pairings: boolean[] = [];
 
-  constructor() { }
+    constructor(private route: ActivatedRoute, private db: DatabaseService) {}
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.route.params.subscribe((params: Params) => {
+            this.tournId = +params["id"];
+        });
 
+        this.finishedTournaments = this.db.loadFinishedTournaments();
+        this.finishedTournaments
+            .pipe(take(1))
+            .subscribe((tourns: tournament[]) => {
+                this.tournName = tourns[this.tournId].name;
+                for (let i = 0; i < tourns[this.tournId].rounds.length; i++) {
+                    this.results.push(true);
+                    this.pairings.push(true);
+                }
+            });
+    }
+
+    onToggleResults(index: number) {
+        this.results[index] = !this.results[index];
+    }
+
+    onTogglePairings(index: number) {
+        this.pairings[index] = !this.pairings[index];
+    }
+
+    ngOnDestroy() {}
 }
